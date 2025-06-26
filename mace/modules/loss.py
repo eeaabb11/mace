d6e6ea6c39fe99fc14ffdca3f4375493c71fe467
Erin <eeaabb11@gmail.com>
2025-06-26 16:28:20 -0700

    Using MSE loss instead of Huber for AtomicTargetsLoss

--- mace/modules/loss.py
+++ mace/modules/loss.py
@@ -217,12 +217,14 @@ def conditional_huber_forces(
     return reduce_loss(se, ddp)
 
 # ------------------------------------------------------------------------------
-# Atomic Targets Loss Function
+# Atomic Targets Loss Function (MSE Version)
 # ------------------------------------------------------------------------------
+print("using MSE loss")
 class AtomicTargetsLoss(torch.nn.Module):
-    def __init__(self, huber_delta=0.01, random_mask=False, random_mask_ratio=None) -> None:
+    def __init__(self, random_mask=False, random_mask_ratio=None) -> None:
         super().__init__()
-        self.huber_loss = torch.nn.HuberLoss(reduction="mean", delta=huber_delta)
+        self.mse_loss = torch.nn.MSELoss(reduction="mean") 
+
         # use random mask or not
         self.random_mask = random_mask
         # preserve `rando_mask_ratio` of points that are not masked
@@ -236,10 +238,10 @@ class AtomicTargetsLoss(torch.nn.Module):
             selected_indices = ones_indices[torch.randperm(len(ones_indices))[:num_ones_to_keep]]
             mask = torch.zeros_like(mask)
             mask[selected_indices] = 1
-        return self.huber_loss(ref["atomic_targets"] * mask, pred["atomic_targets"] * mask) * 1e3
+        return self.mse_loss(ref["atomic_targets"] * mask, pred["atomic_targets"] * mask) * 1e3
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(huber_delta={self.huber_loss.delta:.3f})"
+        return f"{self.__class__.__name__}(loss=MSE)"
 
 # ------------------------------------------------------------------------------
 # Loss Modules Combining Multiple Quantities
